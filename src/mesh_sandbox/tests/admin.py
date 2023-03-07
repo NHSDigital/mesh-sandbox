71787853baa79a6aa63366553317abe9f526bcef
Jim Macdonald <jim.macdonald@aug-data.com>
2023-03-07 16:43:40 +0000

    mesh-1194 change reset to REST delete method

--- src/mesh_sandbox/tests/admin.py
+++ src/mesh_sandbox/tests/admin.py
@@ -20,7 +20,7 @@ def test_reset_canned_store_should_return_bad_request(app: TestClient):
 
     with temp_env_vars(STORE_MODE="canned"):
 
-        res = app.get("/messageexchange/reset")
+        res = app.delete("/messageexchange/reset")
         assert res.status_code == status.HTTP_400_BAD_REQUEST
 
 
@@ -28,7 +28,7 @@ def test_reset_canned_store_with_valid_mailbox_id_should_return_bad_request(app:
 
     with temp_env_vars(STORE_MODE="canned"):
 
-        res = app.get(f"/messageexchange/reset/{_CANNED_MAILBOX1}")
+        res = app.delete(f"/messageexchange/reset/{_CANNED_MAILBOX1}")
         assert res.status_code == status.HTTP_400_BAD_REQUEST
 
 
@@ -36,7 +36,7 @@ def test_reset_memory_store_with_invalid_mailbox_id_should_return_bad_request(ap
 
     with temp_env_vars(STORE_MODE="memory"):
 
-        res = app.get(f"/messageexchange/reset/{uuid4().hex}")
+        res = app.delete(f"/messageexchange/reset/{uuid4().hex}")
         assert res.status_code == status.HTTP_400_BAD_REQUEST
 
 
@@ -44,7 +44,7 @@ def test_reset_file_store_with_invalid_mailbox_id_should_return_bad_request(app:
 
     with temp_env_vars(STORE_MODE="file"):
 
-        res = app.get(f"/messageexchange/reset/{uuid4().hex}")
+        res = app.delete(f"/messageexchange/reset/{uuid4().hex}")
         assert res.status_code == status.HTTP_400_BAD_REQUEST
 
 
@@ -58,7 +58,7 @@ def test_reset_memory_store_should_clear_all_mailboxes(app: TestClient):
         msg_2to1_id = mesh_api_send_message_and_return_message_id(app, _CANNED_MAILBOX2, _CANNED_MAILBOX1)
         assert mesh_api_get_inbox_size(app, _CANNED_MAILBOX1) == 1
 
-        res = app.get("/messageexchange/reset")
+        res = app.delete("/messageexchange/reset")
         assert res.status_code == status.HTTP_200_OK
 
         assert mesh_api_get_inbox_size(app, _CANNED_MAILBOX1) == 0
@@ -88,7 +88,7 @@ def test_reset_memory_store_should_clear_specified_mailbox_only(app: TestClient)
         res = mesh_api_track_message_by_message_id(app, _CANNED_MAILBOX2, msg_2to1_id)
         assert res.json()["messageId"] == msg_2to1_id
 
-        res = app.get(f"/messageexchange/reset/{_CANNED_MAILBOX2}")
+        res = app.delete(f"/messageexchange/reset/{_CANNED_MAILBOX2}")
         assert res.status_code == status.HTTP_200_OK
 
         assert mesh_api_get_inbox_size(app, _CANNED_MAILBOX2) == 0
@@ -124,7 +124,7 @@ def test_reset_file_store_should_clear_all_mailboxes_and_maybe_files(app: TestCl
         assert messages[0] == msg_1to2_id
 
         clear_disk_param = "" if clear_disk is None else f"?clear_disk={clear_disk}"
-        res = app.get(f"/messageexchange/reset{clear_disk_param}")
+        res = app.delete(f"/messageexchange/reset{clear_disk_param}")
         assert res.status_code == status.HTTP_200_OK
 
         assert mesh_api_get_inbox_size(app, _CANNED_MAILBOX1) == 0
@@ -169,7 +169,7 @@ def test_reset_file_store_should_clear_specified_mailbox_only_and_maybe_files(
         assert messages[0] == msg_1to2_id
 
         clear_disk_param = "" if clear_disk is None else f"?clear_disk={clear_disk}"
-        res = app.get(f"/messageexchange/reset/{_CANNED_MAILBOX2}{clear_disk_param}")
+        res = app.delete(f"/messageexchange/reset/{_CANNED_MAILBOX2}{clear_disk_param}")
         assert res.status_code == status.HTTP_200_OK
 
         assert mesh_api_get_inbox_size(app, _CANNED_MAILBOX1) == 1
@@ -197,5 +197,5 @@ def test_reset_file_store_should_not_error_if_folder_does_not_exist_yet(
         assert not os.path.exists(inbox_folder)
 
         clear_disk_param = "" if clear_disk is None else f"?clear_disk={clear_disk}"
-        res = app.get(f"/messageexchange/reset/{_CANNED_MAILBOX2}{clear_disk_param}")
+        res = app.delete(f"/messageexchange/reset/{_CANNED_MAILBOX2}{clear_disk_param}")
         assert res.status_code == status.HTTP_200_OK
