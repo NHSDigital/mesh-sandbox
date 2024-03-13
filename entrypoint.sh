e52fb3e6efb1e3df404065ddbb9cd4171eb69eb7
Patrik Norlin <113345270+patriknorlin@users.noreply.github.com>
2024-03-13 09:41:53 +0100

    add github workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 SSL="${SSL-no}"
 SSL_CRTFILE="${SSL_CRTFILE-/tmp/server-cert.pem}"
 SSL_KEYFILE="${SSL_KEYFILE-/tmp/server-cert.key}"
+MUTUAL_TLS_CA="${MUTUAL_TLS_CA/tmp/ca.crt}"
 
 if [[ -z "${PORT}" ]]; then
   if [[ "${SSL}" == "yes" ]]; then
@@ -18,7 +19,12 @@ if [[ "${SSL}" == "yes" ]]; then
     openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout "${SSL_KEYFILE}" -out "${SSL_CRTFILE}"  -subj "/C=GB/O=nhs/OU=local/CN=localhost"
   fi
 
-  exec uvicorn mesh_sandbox.api:app --host "0.0.0.0" --port "${PORT}" --workers 1 --ssl-certfile "${SSL_CRTFILE}" --ssl-keyfile "${SSL_KEYFILE}"
+  if [ -f "${MUTUAL_TLS_CA}" ] && [ -f "${SSL_CRTFILE}" ] && [ -f "${SSL_KEYFILE}" ]; then
+    exec uvicorn mesh_sandbox.api:app --host "0.0.0.0" --port "${PORT}" --workers 1 --ssl-certfile "${SSL_CRTFILE}" --ssl-keyfile "${SSL_KEYFILE}" --ssl-ca-certs "${MUTUAL_TLS_CA}" --ssl-cert-reqs 2
+  else
+    exec uvicorn mesh_sandbox.api:app --host "0.0.0.0" --port "${PORT}" --workers 1 --ssl-certfile "${SSL_CRTFILE}" --ssl-keyfile "${SSL_KEYFILE}"
+  fi
+
 else
   exec uvicorn mesh_sandbox.api:app --host "0.0.0.0" --port "${PORT}" --workers 1
 fi
