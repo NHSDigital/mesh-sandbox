e744c192ddd1a8757a6aa86b3f96df1af312e3dc
James <james.bradley7@nhs.net>
2026-01-16 09:05:34 +0000

    mesh-2092: deliberately break test

--- src/mesh_sandbox/tests/java_client_tests.py
+++ src/mesh_sandbox/tests/java_client_tests.py
@@ -155,7 +155,7 @@ def find_sent_message_id(ctl_file: str) -> str:
     parser = etree.XMLParser(remove_blank_text=True, resolve_entities=False)
     root = cast(_ElementTree, etree.parse(ctl_file, parser)).getroot()
     message_id = root.find("DTSId")
-    assert message_id is not None
+    assert message_id == "NOT THE MESSAGE ID BREAKING"
     return cast(str, message_id.text)
 
 
